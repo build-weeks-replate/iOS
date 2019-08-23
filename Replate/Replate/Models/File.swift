599362e72f23e4514119a14db54da1c270344dc9
Vici Shaweddy <vshaweddy@gmail.com>
2019-08-22 23:06:20 -0400

    adding dashboard and detail view

---
+++ Replate/Replate/Models/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Replate
+//
+//  Created by Vici Shaweddy on 8/22/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
+
+struct FoodItem {
+    let name: String
+}
